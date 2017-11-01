27fc10155d02f8168bc10c8b7b1b69ddaf6d94e5
Michal Cizevskij <michael.chizhevskiy@hotmail.com>
2017-11-01 13:07:10 +0100

    with help of Andre

--- Obstacle.js
+++ Obstacle.js
@@ -1,109 +1,41 @@
 'use strict';
 
-function Obstacle(positionLeftX, positionLeftY) {
+function Obstacle(ctx, x, width, speed) {
   var self = this;
-  self.canvas = document.getElementById('game');
-  self.ctx = self.canvas.getContext('2d');
-  //this.position = position;
-  self.width = self.canvas.width;
-  self.height = self.canvas.height;
-  //--------------------
-  //self.randomPosition = Math.floor(Math.random() * 2);
 
-  self.rHeight = self.randomizeHeight();
-  if (self.rHeight > 50) {
-    self.rHeight = 30;
-  } else if (self.rHeight < 3) {
-    self.rHeight = 5;
-  }
+  self.ctx = ctx;
 
-  self.rWidthRight = self.randomizeWidth();
-  self.rWidthLeft = self.randomizeWidth();
-  self.heightRight = self.rHeight;
-  self.heightLeft = self.rHeight;
-  // X & Y Postion RIGHT //
-  self.positionRightX = (self.width - 40 - self.rWidthRight);
-  self.positionRightY = 0;
-  // X & Y Postion LEFT //
-  self.positionLeftX = 40;
-  self.positionLeftY = 0;
+  self.gone = false;
 
-  self.leftObastacle = null;
-  self.rightObstacle = null;
+  self.x = x;
+  self.width = width;
+  self.speed = speed;
 
-  self.speed = 0;
-
-  //---LEFT---//
-
-
-  //---RIGHT---//
-
-  var positionRightX = self.positionRightX;
-  var positionRightY = self.positionRightY;
-
+  self.y = -50;
+  self.height = 10;
 }
 
 
+Obstacle.prototype.setSpeed = function(speed) {
+  var self = this;
 
-//-----DRAWING------//
+  self.speed = speed;
+};
+
+Obstacle.prototype.update = function() {
+  var self = this;
+
+  self.y = self.y + self.speed;
+
+  if (self.y > self.ctx.canvas.height) {
+    self.gone = true;
+  }
+};
+
 
 Obstacle.prototype.draw = function() {
   var self = this;
-  self.ctx.clearRect(0, self.height, self.width, self.height);
-  self.drawRightObject();
-  self.drawLeftObject();
 
-  window.requestAnimationFrame(function() {
-    self.draw();
-  });
+  self.ctx.fillStyle = 'green';
+  self.ctx.fillRect(self.x, self.y, self.width, self.height);
 };
-
-//-------DRAWING-BOTH-OBSTACLES-------//
-Obstacle.prototype.drawLeftObject = function() {
-  var self = this;
-
-  self.ctx.fillStyle = "rgb(0, 204, 153)";
-  self.ctx.fillRect(self.positionLeftX, self.speed, self.rWidthLeft, self.heightLeft);
-};
-
-Obstacle.prototype.drawRightObject = function() {
-  var self = this;
-  self.ctx.fillStyle = "rgb(0, 204, 153)";
-  self.ctx.fillRect(self.positionRightX, self.speed, self.rWidthRight, self.heightRight);
-};
-
-//----UPDATE-OBSTACLES-----//
-Obstacle.prototype.updateObstacle = function() {
-  var self = this;
-  self.speed += 2;
-
-  //---ONLY IF RUN AGAIN THE SAME OBSTACLE---//
-  // self.rWidthRight = self.randomize();
-  // self.positionRightX = (self.width - 40 - this.rWidthRight);
-  // self.rWidthLeft = self.randomize();
-  // self.positionLeftX = 40;
-};
-
-Obstacle.prototype.randomizeWidth = function(width) {
-  var self = this;
-  width = Math.floor(Math.random() * self.width / 2);
-  return width;
-};
-
-
-Obstacle.prototype.randomizeHeight = function(height) {
-  var self = this;
-  height = Math.floor(Math.random() * (self.height * 0.05));
-  return height;
-};
-
-
-
-
-// self.enemiesIntervalId = window.setInterval(function () {
-//       var enemy = new Enemy(self.ctx, self.width, self.height);
-//       self.objects.push(enemy);
-//       enemy.onDie(function () {
-//         self.destroyObject(enemy);
-//       });
-//     }, ENEMIES_MSEC);
--- game.js
+++ game.js
@@ -9,18 +9,29 @@ function Game(container) {
   self.height = self.canvas.height;
   self.width = self.canvas.width;
 
-  self.obstacle = new Obstacle();
+  self.obstacles = [];
 
+  self.obstacleMinWidth = 50;
+  self.obstacleWidthPerLevel = 30;
+
+  self.speedPerLevel = 0.2;
+
+  self.backgroundMoving = false;
   self.leftWallX = 30;
   self.rightWallX = 360;
   self.wallWidth = 10;
-  self.plaverWidth = 20;
-  self.plaverHeight = 40;
-  self.bottomBorderY = self.height - (self.plaverHeight / 2);
+  self.playerWidth = 20;
+  self.playerHeight = 40;
+  self.groundY = self.height - (self.playerHeight / 2);
 
   self.playerJumpingRight = null;
   self.playerJumpingLeft = null;
   self.isRunning = false;
+
+  self.levelUpInterval = 5000;
+  self.obstacleInterval = 5000;
+
+  self.level = 1;
 
   // -- creating the start button -- //
 
@@ -38,54 +49,100 @@ function Game(container) {
     button.style.display = "none";
   });
 
-  //------SPAWNING INTERVAL------//
-  self.intervalSpawn = window.setInterval(function() {
 
-    var newObstacle = new Obstacle();
+  // ---- LEVEL UP INTERVAL ---- //
+
+  self.levelUpIntervalId = window.setInterval(function() {
+    self.level++;
+
+    console.log("LEVEL UP", self.level);
+
+    self.levelUpWarningCounter = 50;
+
+    self.obstacles.forEach(function(obstacle) {
+      obstacle.setSpeed(1 + self.level * self.speedPerLevel);
+    });
+
+    // @todo make obstacleInterval smaller (multiply by 0.9)
+    // @todo clear the obstacle interval
+    // @todo set the obstacle interval again
+
+  }, self.levelUpInterval);
 
 
-
-  });
 }
 
+//------SPAWNING INTERVAL------//
+
+Game.prototype.setObstacleInterval = function() {
+  var self = this;
+
+  self.obstacleIntervalId = window.setInterval(function() {
+
+    var left = Math.round(Math.random());
+    var width = self.randomizeObstacleWidth();
+    var speed = 1 + self.level * self.speedPerLevel;
+    var x;
+    if (left) {
+      x = self.leftWallX + self.wallWidth;
+    } else {
+      x = self.rightWallX - width;
+    }
+
+    var newObstacle = new Obstacle(self.ctx, x, width, speed);
+    self.obstacles.push(newObstacle);
+  }, self.obstacleInterval);
+
+};
+
+
+// ----- UTILITY FUNCTIONS -----
+
+
+Game.prototype.randomizeObstacleWidth = function() {
+  var self = this;
+  var width = self.obstacleMinWidth + Math.floor(Math.random() * self.level * self.obstacleWidthPerLevel);
+  return width;
+};
 
 // ----- UPDATE STATE -----
+
+
 Game.prototype.updatePlayer = function() {
   var self = this;
   if (self.playerJumpingRight === true) {
     self.isRunning = true;
-    self.plaverX++;
-    console.log("right ", self.plaverX);
-    if (self.plaverX >= (self.rightWallX - self.plaverWidth)) {
+    self.playerX += 2 + self.level * self.speedPerLevel;
+    if (self.playerX >= (self.rightWallX - self.playerWidth)) {
 
       self.playerJumpingRight = null;
       self.isRunning = false;
-      self.plaverX = self.rightWallX - self.plaverWidth;
+      self.playerX = self.rightWallX - self.playerWidth;
     }
   }
 
   if (self.playerJumpingLeft === true) {
     self.isRunning = true;
-    self.plaverX--;
-    console.log("left ",
-      self.plaverX);
-    if (self.plaverX <= self.leftWallX + self.wallWidth) {
+    self.playerX -= 2 + self.level * self.speedPerLevel;
+    if (self.playerX <= self.leftWallX + self.wallWidth) {
 
       self.playerJumpingLeft = null;
       self.isRunning = false;
-      self.plaverX = (self.leftWallX + self.wallWidth);
+      self.playerX = (self.leftWallX + self.wallWidth);
     }
   }
 };
 
-Game.prototype.updateBorder = function() {
+Game.prototype.detectCollision = function() {
+  // @todo if blab bla or bla bla or bla this.playerCollision = true;
+};
+
+Game.prototype.updateGround = function() {
   var self = this;
-  //-----not sure about this when create many obstacles the border may appear again!!!!!----//
-  if (self.obstacle.speed > (self.height / 3)) {
-    self.bottomBorderY += 1;
-    if (self.bottomBorderY === self.height) {
-      self.bottomBorderY += 0;
-    }
+  self.groundY += 1;
+  if (self.groundY > self.height) {
+    self.backgroundMoving = true;
+    self.setObstacleInterval();
   }
 };
 
@@ -95,7 +152,7 @@ Game.prototype.updateBorder = function() {
 Game.prototype.drawPlayer = function() {
   var self = this;
   self.ctx.fillStyle = "rgb(50,50,250)";
-  self.ctx.fillRect(self.plaverX, self.plaverY, self.plaverWidth, self.plaverHeight);
+  self.ctx.fillRect(self.playerX, self.playerY, self.playerWidth, self.playerHeight);
 };
 
 Game.prototype.drawWalls = function() {
@@ -107,29 +164,54 @@ Game.prototype.drawWalls = function() {
 
 };
 
-Game.prototype.drawBorder = function() {
+Game.prototype.drawGround = function() {
   var self = this;
   self.ctx.fillStyle = "rgb(102, 0, 102)";
-  self.ctx.fillRect(0, self.bottomBorderY, self.width, self.plaverHeight / 2);
+  self.ctx.fillRect(0, self.groundY, self.width, self.playerHeight / 2);
 };
 
 
 //--------DRAW-------//
 Game.prototype.draw = function() {
   var self = this;
+
+  //-----movement and redrawning of objects-----//
+
+  if (self.playerCollision) {
+    // @todo update collision animation
+  } else {
+    self.updatePlayer();
+    if (!self.backgroundMoving) {
+      self.updateGround();
+    }
+    self.obstacles.forEach(function(obstacle) {
+      obstacle.update();
+    });
+
+    self.detectCollision(); // self.playerCollision = true;
+  }
+
   //---clearing canvas----//
   self.ctx.clearRect(0, 0, self.width, self.height);
-  //-----movement and redrawning of objects-----///
-  self.updatePlayer();
-  self.updateBorder();
-  self.obstacle.draw();
+
+  // ---redraw--- //
   self.drawWalls();
-  self.drawBorder();
-  self.drawPlayer();
-  self.obstacle.updateObstacle();
+  if (!self.backgroundMoving) {
+    self.drawGround();
+  }
+
+  if (self.playerCollision) {
+    // @todo draw collision animation
+  } else {
+    self.drawPlayer();
+    self.obstacles.forEach(function(obstacle) {
+      obstacle.draw();
+    });
+  }
+
+  // request the browser to update/draw again as soon as possible
   window.requestAnimationFrame(function() {
     self.draw();
-
   });
 };
 
@@ -140,8 +222,8 @@ Game.prototype.start = function() {
   var self = this;
 
   // prepare the game status
-  self.plaverX = self.leftWallX + self.wallWidth;
-  self.plaverY = self.height - (self.plaverHeight * 1.5);
+  self.playerX = self.leftWallX + self.wallWidth;
+  self.playerY = self.height - (self.playerHeight * 1.5);
   self.playerJumpingRight = false;
 
 
